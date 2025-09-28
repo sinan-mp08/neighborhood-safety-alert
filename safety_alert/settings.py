f7a347174cd9d8cab6462c8ef2fbc25f7001786e
Mohammed Sinan MP <mohammedsinanmullapally@gmail.com>
2025-09-28 13:58:19 +0530

    Fix Render DB connection and auth tables

--- safety_alert/settings.py
+++ safety_alert/settings.py
@@ -76,8 +76,6 @@ WSGI_APPLICATION = "safety_alert.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 import dj_database_url
 
-
-
 DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL')
